7f6a7120d7077c531c3503da5b9217fbf87da2d4
Zekiiiee <chloeijavier22@gmail.com>
2026-04-22 09:31:42 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 
 
 // Dev or Prod
-define('IS_DEV', true); // set to false when online
+define('IS_DEV', true); 
 
-// Database Config (InfinityFree)
+
 define('DB_DRIVER', 'mysql'); 
-define('DB_HOST', 'localhost'); 
+define('DB_HOST', 'sql12.freesqldatabase.com'); 
 define('DB_PORT', '3306'); 
-define('DB_NAME', 'javier_chloei'); 
-define('DB_USERNAME', 'root'); 
-define('DB_PASSWORD', ''); 
+define('DB_NAME', 'sql12823918'); 
+define('DB_USERNAME', 'sql12823918'); 
+define('DB_PASSWORD', 'dUZUAAabye'); 
 define('DB_CHARSET', 'utf8mb4'); 
 define('DB_PREFIX', ''); 
 define('DB_PATH', '');
