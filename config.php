2c8f19ae4497b5fb29a1edb9e25e65bf71d9198a
Zekiiiee <chloeijavier22@gmail.com>
2026-04-30 09:53:28 +0800

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ define('DB_HOST', 'sql12.freesqldatabase.com');
 define('DB_PORT', '3306'); 
 define('DB_NAME', 'sql12824903'); 
 define('DB_USERNAME', 'sql12824903'); 
-define('DB_PASSWORD', 'uYkl2ZnwCv'); 
+define('DB_PASSWORD', 'uYkl2ZNwCv'); 
 define('DB_CHARSET', 'utf8mb4'); 
 define('DB_PREFIX', ''); 
 define('DB_PATH', '');
